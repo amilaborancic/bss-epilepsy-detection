db30ab5342596ceae045b7b90e3fd68bf1046dfc
Mahira B <mbuturovic1@etf.unsa.ba>
2020-07-10 02:49:15 +0200

    dodani dijagnoza i uzroci epilepsije,izmjena varijable Klasa u 0-nema i 1-ima napad,kreiranje modela

--- dataset-prune-script.R
+++ dataset-prune-script.R
@@ -1,6 +1,6 @@
 #zakomentarisati
 setwd("C://Users/USER/Desktop/bss-epilepsy-detection")
-
+library(e1071)
 
 #ucitavanje dataseta
 data_original <- read.csv("EpilepticSeizureRecognitionORIGINAL.csv", header=T)
@@ -14,3 +14,14 @@ data_reduced <- data_reduced[,-1]
 
 #spasimo novi dataset
 write.csv(data_reduced,'EpilepticSeizureRecognitionREDUCED.csv')
+
+
+data_reduced$Klasa[data_reduced$Klasa==2]<-0
+data_reduced$Klasa[data_reduced$Klasa==3]<-0
+data_reduced$Klasa[data_reduced$Klasa==4]<-0
+data_reduced$Klasa[data_reduced$Klasa==5]<-0
+
+data_reduced["Klasa"] <- lapply(data_reduced["Klasa"], as.factor)
+data<-data_reduced[,c("X1","X2","Klasa")]
+svmfit = svm(Klasa ~ ., data = data, kernel = "linear", cost = 10, scale = FALSE)
+#plot(svmfit,data)
