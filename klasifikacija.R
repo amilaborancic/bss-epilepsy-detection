0db5994c872c03c415ee1c1bd4f1f51a64fa92af
Mahira B <mbuturovic1@etf.unsa.ba>
2020-07-22 16:52:07 +0200

    konfuzija

--- klasifikacija.R
+++ klasifikacija.R
@@ -50,5 +50,10 @@ nm_train <- normalize(train_data[,!colnames(train_data) %in% c("key","Klasa")])
 nm_test <- normalize(test_data[,!colnames(test_data) %in% c("key","Klasa")])
 nm_train$Klasa<-train_data[,c("Klasa")]
 nm_test$Klasa <- test_data[,c("Klasa")]
-
+#konfuzijska matrica
+tune.out <- tune(svm,Klasa~.,data=nm_train, kernel ="linear", ranges=list(cost=c(0.01,0.1,1,10)))
+svm_model <- tune.out$best.model
+svm_predict <- predict(svm_model, nm_test)
+svm_model <- tune.out$best.model
+svm_table <- confusionMatrix(svm_predict, nm_test$Klasa)
 
